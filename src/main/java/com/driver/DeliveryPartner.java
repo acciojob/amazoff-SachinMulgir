ee53da1f3faa5c4a4ab903d5d838f0af931973a6
SachinMulgir <smulgir636@gmail.com>
2023-05-14 21:44:09 +0530

    submission 10

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 
 
 @AllArgsConstructor
-@NoArgsConstructor
 public class DeliveryPartner {
 
     private String id;
@@ -18,6 +17,13 @@ public class DeliveryPartner {
     public DeliveryPartner(String id) {
         this.id = id;
         this.numberOfOrders = 0;
+    }
+    public DeliveryPartner() {
+    }
+
+    public DeliveryPartner(String id, int numberOfOrders) {
+        this.id = id;
+        this.numberOfOrders = numberOfOrders;
     }
 
     public String getId() {
